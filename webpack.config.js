099a8757bc164f665fc7c6c53c94c7c5c5a66cf4
Ryota Yamamoto <ryota-yamamoto@retty.me>
2018-08-12 13:41:55 +0900

    remove lib file

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
             presets: [
               ['@babel/preset-env', {
                 targets: {
+                  node: '8.0.0',
                   browsers: [
                     'last 1 version',
                     '> 5%',
